70d5cab06924a43128b3b001af088caac5c28ad8
resszermate <materesszer2@gmail.com>
2024-12-13 18:13:43 +0100

    readded first solution

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,31 +4,39 @@
     {
         static void Main(string[] args)
         {
-            List<string> list = File.ReadAllLines("input.txt").ToList();
+            List<string> file = File.ReadAllLines("input.txt").ToList();
 
-            List<int> nums1 = new List<int>();
-            List<int> nums2 = new List<int>();
+            List<int> numbers1 = new List<int>();
+            List<int> numbers2 = new List<int>();
 
-            foreach (string str in list)
+            foreach (string line in file)
             {
-                string[] line = System.Text.RegularExpressions.Regex.Split(str, @"\s{2,}");
-
-
-                nums1.Add(int.Parse(line[0]));
-                nums2.Add(int.Parse(line[1]));
+                List<string> splitLine = System.Text.RegularExpressions.Regex.Split(line, @"\s{2,}").ToList();
+                numbers1.Add(int.Parse(splitLine[0]));
+                numbers2.Add(int.Parse(splitLine[1]));
             }
 
-            nums1.Sort();
-            nums2.Sort();
+            numbers1.Sort();
+            numbers2.Sort();
 
             int totalDistance = 0;
 
-            for (int i = 0; i < nums1.Count; i++)
+            for (int i = 0; i < numbers1.Count; i++)
             {
-                totalDistance += Math.Abs(nums1[i] - nums2[i]);
+                totalDistance += Math.Abs(numbers1[i] - numbers2[i]);
             }
 
             Console.WriteLine(totalDistance);
+
+            int simScore = 0;
+
+            for (int i = 0; i < numbers1.Count; i++)
+            {
+                int appearsTimes = numbers2.Where(x => x == numbers1[i]).Count();
+                simScore += appearsTimes * numbers1[i];
+            }
+
+            Console.WriteLine(simScore);
         }
     }
 }
