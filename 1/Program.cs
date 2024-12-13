1aa2bce3ec43a657c698bb5ceac6b7152cdf3988
materesszer <materesszer2@gmail.com>
2024-12-13 17:52:00 +0100

    day 2 half 1

---
+++ 1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = File.ReadAllLines("input.txt").ToList();
+
+            List<int> nums1 = new List<int>();
+            List<int> nums2 = new List<int>();
+
+            foreach (string str in list)
+            {
+                string[] line = System.Text.RegularExpressions.Regex.Split(str, @"\s{2,}");
+
+
+                nums1.Add(int.Parse(line[0]));
+                nums2.Add(int.Parse(line[1]));
+            }
+
+            nums1.Sort();
+            nums2.Sort();
+
+            int totalDistance = 0;
+
+            for (int i = 0; i < nums1.Count; i++)
+            {
+                totalDistance += Math.Abs(nums1[i] - nums2[i]);
+            }
+
+            Console.WriteLine(totalDistance);
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            string[] data = File.ReadAllLines("input.txt");
+            int safeReports = 0;
+            foreach (string line in data)
+            {
+                int[] values = line.Split(' ').Select(int.Parse).ToArray();
+
+                if (values[0] > values[1])
+                {
+                    int counter = 0;
+                    while (counter < values.Length - 1 && values[counter] > values[counter + 1] && values[counter] - values[counter + 1] >= 1 && values[counter] - values[counter + 1] <= 3)
+                    {
+                        counter++;
+                    }
+                    if (counter >= values.Length - 1)
+                    {
+                        safeReports++;
+                    }
+                }
+                else
+                {
+                    int counter = 0;
+                    while (counter < values.Length - 1 && values[counter] < values[counter + 1] && values[counter + 1] - values[counter] >= 1 && values[counter + 1] - values[counter] <= 3)
+                    {
+                        counter++;
+                    }
+                    if (counter >= values.Length - 1)
+                    {
+                        safeReports++;
+                    }
+
+                }
+
+
+            }
+            Console.WriteLine(safeReports);
+
+        }
+
+        static void Part2()
+        {
+
+            string[] data = File.ReadAllLines("input.txt");
+            int safeReports = 0;
+            foreach (string line in data)
+            {
+                int[] values = line.Split(' ').Select(int.Parse).ToArray();
+
+                if (values[0] > values[1])
+                {
+                    int counter = 0;
+                    while (counter < values.Length - 1 && values[counter] > values[counter + 1] && values[counter] - values[counter + 1] >= 1 && values[counter] - values[counter + 1] <= 3)
+                    {
+                        counter++;
+                    }
+                    if (counter >= values.Length - 1)
+                    {
+                        safeReports++;
+                    }
+                    else
+                    {
+                        // Try removing each element once and see if removing it makes the report safe
+                    }
+                }
+                else
+                {
+                    int counter = 0;
+                    while (counter < values.Length - 1 && values[counter] < values[counter + 1] && values[counter + 1] - values[counter] >= 1 && values[counter + 1] - values[counter] <= 3)
+                    {
+                        counter++;
+                    }
+                    if (counter >= values.Length - 1)
+                    {
+                        safeReports++;
+                    }
+
+                }
+            }
+        }
+    }
+}
