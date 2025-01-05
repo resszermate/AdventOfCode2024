b60f0143b23335cc0c8d2bda0da906f272bd0d00
materesszer <materesszer2@gmail.com>
2025-01-05 01:09:09 +0100

    input.txt

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace _4
 {
+    class WordSearch
+    {
+        string wordToSearch;
+        char[][] grid;
+
+        public WordSearch(string wordToSearch, char[][] grid)
+        {
+            this.wordToSearch = wordToSearch;
+            this.grid = grid;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
